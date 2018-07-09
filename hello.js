a288d593198e7c3f47e5c95b01993badce13fe03
Student <student@Admins-MacBook-Air.local>
2018-07-09 12:07:52 -0700

    new new new

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-// Author: FirstName LastName
+// Author: Shannon Harveys
+
+console.log('Hello world!!!');
+
+console.log('I found out 1 + 1 = 2!!!');
+
+console.log('That is so awesome!!!!')
+
+console.log(':)')
